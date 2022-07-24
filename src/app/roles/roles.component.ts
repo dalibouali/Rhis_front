e7b6846b60270a4ff739bb456c61ecd4025f0ef0
houssemeddin07 <houssemeddin.loudhabachi@esprit.tn>
2022-07-24 22:53:07 +0100

    houssem's commit

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -18,6 +18,7 @@ export class RolesComponent implements OnInit {
 public droits:Droit[]=[];
 public roles:Role[]=[];
 public ecrans:Ecran[]=[];
+public deleteDroit:Droit|null=null;
  
 
 
@@ -44,19 +45,8 @@ public ecrans:Ecran[]=[];
   public getRoles():void{
     this.roleservice.getRoles().subscribe(
       (response:Role[])=>{this.roles=response;
-      //  console.log(this.roles)
-       // console.log(this.roles[0].name)
-      
-       //console.log(this.roles[0]);
-      /*
-       const essr=this.roles ;
-   for (var role in essr)
-    console.log(role)
-    */
    for (var role of this.roles)
    console.log(role.name)
-    
-   //console.log(response);
       },
       (error:HttpErrorResponse)=>{alert(error.message);}
     )
@@ -66,40 +56,31 @@ public ecrans:Ecran[]=[];
   public getEcrans():void{
     this.ecranservice.getEcrans().subscribe(
       (response:Ecran[])=>{this.ecrans=response;
-       // console.log(this.ecrans[0]);
-       // console.log(this.ecrans[0].EcranList[0].cum)
       },
       (error:HttpErrorResponse)=>{alert(error.message);}
     )
   }
 
-  public checkEcranMAA( droit:number){
+  public OnDeleteDroit(droit:Droit|null):void{
+    if(droit){
+      this.droitservice.deleteDroit(droit.id).subscribe(
+        (response:any)=>{
+          console.log(response)
+          this.getDroits();
+          this.getRoles();
+          this.getEcrans();
+        },
+        (error:HttpErrorResponse)=>{
+          alert(error.message)
+          console.log('error')
+          
+        }
+      )
+    }
 
-    if (droit===7)
-    return false;
-    else
-    return true;
   }
 
-  
-  public checkEcranAA(droit:number){
-
-    if (droit===3)
-    return false;
-    else
-    return true;
-  }
-  
-  public checkEcranA(droit:number){
-
-    if (droit===1)
-    return false;
-    else
-    return true;
-  }
-
-  /*
-  public onOpenModal(user:User|null,mode:string):void{
+  public onOpenModal(droit:Droit|null,mode:string):void{
 
     const container= document.getElementById('mycontainer');
     const button=document.createElement('button');
@@ -107,20 +88,18 @@ public ecrans:Ecran[]=[];
     button.style.display='none';
     button.setAttribute('data-toggle','modal');
     if(mode==='add'){
-      button.setAttribute('data-target','#addUserModal')
+      button.setAttribute('data-target','#addDroitModal')
+
     }
     if(mode==='delete'){
-      button.setAttribute('data-target','#deleteUserModal')
-      this.deleteUser=user;
+      button.setAttribute('data-target','#deleteDroitModal')
+      this.deleteDroit=droit;
     }
     if(mode==='edit'){
-      this.editUser=user;
-      button.setAttribute('data-target','#updateUserModal')
+     // this.editUser=user;
+      button.setAttribute('data-target','#updateDroitModal')
     }
     container?.appendChild(button);
     button.click();
-    //console.log(this.deleteUser?.firstName)
 }
-*/
-
 }
